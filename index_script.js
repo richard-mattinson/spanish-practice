3230e9eedd673a14792c4221d8fb72c14ef40757
Richard Mattinson <richard.m@ttinson.net>
2024-08-10 16:58:59 +0100

    emotions!

--- index_script.js
+++ index_script.js
@@ -2,29 +2,33 @@
 
 const titleText = document.querySelector("#title_text");
 
-const verbTextQuestion = document.querySelector("#verb_text_question");
 
-const verbRedCross = document.querySelector("#red_cross");
-const verbGreenCheck = document.querySelector("#green_check");
+const navbarButtons = document.querySelectorAll(".navbar_buttons")
+const navbarVerb = document.querySelector("#verb_button")
+const navbarEmotion = document.querySelector("#emotions_button")
+
+const questionText = document.querySelector("#verb_text_question");
+
+const redCross = document.querySelector("#red_cross");
+const greenCheck = document.querySelector("#green_check");
 
 const correctText = document.querySelector("#correct_text");
 const correctCounter = document.querySelector("#correct_counter");
 const correctTen = document.querySelector("#correct_ten");
 
-const verbTextAnswer = document.querySelector("#verb_text_answer");
-const verbTitle = document.querySelectorAll(".verb_title");
+const answerText = document.querySelector("#verb_text_answer");
 
 const tableBody = document.querySelector("#table_body");
-
-// const verbButtonAnswer = document.querySelector("#verb_button_answer");
 
 /////////////////////////////// VARIABLE STORAGE ///////////////////////////////
 
 const state = {
     color: "red",
     correctCounter: 0,
+    currentTab: "verbs",
     errorCounter: 0,
-    verbIndex: "",
+    errorRegister: false,
+    questionIndex: "",
 };
 
 const verbs = [
@@ -52,6 +56,34 @@ const verbs = [
     ["Visitar", ["Visit"]],
 ];
 
+const emotions = [
+  ["Aburrido", ["Bored"]],
+  ["Alegre", ["Joyful", "Happy"]],
+  ["Asustado", ["Scared"]],
+  ["Avergonzado", ["Embarrassed"]],
+  ["Confundido", ["Confused"]],
+  ["Contento", ["Pleased", "Happy"]],
+  ["Deprimido", ["Depressed"]],
+  ["Descuidado", ["Thoughtless"]],
+  ["Enamorado", ["In Love"]],
+  ["Enfadado", ["Angry", "Mad"]],
+  ["Enfermo", ["Unwell"]],
+  ["Emocionado", ["Excited"]],
+  ["Feliz", ["Happy"]],
+  ["Molesto", ["Annoyed"]],
+  ["Nervioso", ["Nervous"]],
+  ["Orgulloso", ["Proud"]],
+  ["Pensativo", ["Thoughtful"]],
+  ["Preocupado", ["Worried"]],
+  ["Saludable", ["Healthy"]],
+  ["Seguro", ["Confident"]],
+  ["Serio", ["Serious"]],
+  ["Sorprendido", ["Suprised"]],
+  ["Tranquilo", ["Relaxed"]],
+  ["Tonto", ["Silly"]],
+  ["Triste", ["Sad"]],
+];
+
 /////////////////////////////// EVENT LISTENERS ///////////////////////////////
 // verbButtonAnswer.addEventListener("click", () => {
 //     const answer = verbTextAnswer.value
@@ -59,52 +91,99 @@ const verbs = [
 // })
 
 titleText.addEventListener("click", () => {
-    state.correctCounter = 0
-    state.errorCounter = 0
-    titleText.textContent = "¿Richard habla español?";
-    correctText.textContent = "Correcto";
-    correctCounter.textContent = state.correctCounter
-    correctText.style.color = "black";
-    correctCounter.style.color = "black";
-    correctTen.style.color = "black";
-    updateVerbText()
-    while (tableBody.hasChildNodes()) {
-        tableBody.removeChild(tableBody.firstChild)
-    }
+    resetTab()
 })
 
-verbTextQuestion.addEventListener("click", () => {
-    verbTextQuestion.textContent = verbs[state.verbIndex][1][0] + "...";
-    verbTextQuestion.style.color = "orange";
+navbarVerb.addEventListener("click", () => {
+  state.currentTab = "verbs"
+  setNavbarStyling()
+  resetTab()
+})
+
+navbarEmotion.addEventListener("click", () => {
+  state.currentTab = "emotions"
+  setNavbarStyling()
+  resetTab()
+})
+
+questionText.addEventListener("click", () => {
+  // shows answer on click briefly (for idiots)
+  let currentQuestion
+  switch (state.currentTab) {
+    case "verbs":
+      questionText.textContent = verbs[state.questionIndex][1][0] + "...";
+      currentQuestion = verbs[state.questionIndex][0]
+      break;
+    case "emotions":
+      questionText.textContent = emotions[state.questionIndex][1][0] + "...";
+      currentQuestion = emotions[state.questionIndex][0]
+      break;
+  }
+    questionText.style.color = "orange";
     state.errorCounter++;
     setTimeout(() => {
-        verbTextQuestion.textContent = verbs[state.verbIndex][0];
-        verbTextQuestion.style.color = "black";
+        questionText.textContent = currentQuestion;
+        questionText.style.color = "black";
     }, 2000);
 });
 
-verbTextAnswer.addEventListener("keydown", (event) => {
+answerText.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-        const answer = verbTextAnswer.value;
+        const answer = answerText.value;
         checkAnswer(answer);
     }
 });
 
 /////////////////////////////// FUNCTIONS ///////////////////////////////
 
-function updateVerbText() {
-    let verbIndex = Math.floor(Math.random() * verbs.length);
-    let randomVerb = verbs[verbIndex];
-    console.log("random verb", randomVerb);
-    state.verbIndex = verbIndex;
-    verbTextQuestion.textContent = randomVerb[0];
-    state.answerSummary = randomVerb;
+function resetTab() {
+  state.correctCounter = 0;
+  state.errorCounter = 0;
+  titleText.textContent = "¿Richard habla español?";
+  correctText.textContent = "Correcto";
+  correctCounter.textContent = state.correctCounter;
+  correctText.style.color = "black";
+  correctCounter.style.color = "black";
+  correctTen.style.color = "black";
+  updateQuestionText();
+  while (tableBody.hasChildNodes()) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+}
+
+function setNavbarStyling() {
+  navbarButtons.forEach((element) => element.classList.remove("button_active"))
+  switch (state.currentTab) {
+    case "verbs":
+      navbarVerb.classList.add("button_active");
+      break;
+    case "emotions":
+      navbarEmotion.classList.add("button_active");
+      break;
+  }
+}
+
+function updateQuestionText() {
+  let randomQuestion
+    switch (state.currentTab) {
+      case "verbs":
+        state.questionIndex = Math.floor(Math.random() * verbs.length);
+        randomQuestion = verbs[state.questionIndex];
+        break;
+      case "emotions": 
+        state.questionIndex = Math.floor(Math.random() * emotions.length);
+        randomQuestion = emotions[state.questionIndex];
+        break;
+    }
+    questionText.textContent = randomQuestion[0];
+    state.answerSummary = randomQuestion;
 }
 
 function updateSummaryTable() {
   const newRow = document.createElement("tr");
   const newSpanishCell = document.createElement("td");
   const newEnglishCell = document.createElement("td");
+
   newSpanishCell.textContent = state.answerSummary[0];
   newSpanishCell.setAttribute("class", "summary_spanish")
   newSpanishCell.setAttribute("id", state.answerSummary[0]);
@@ -112,7 +191,15 @@ function updateSummaryTable() {
     const word = event.target.id
     playSpanish(word)
   })
+  if (state.errorRegister) {
+    newSpanishCell.style.background = "red";
+    newSpanishCell.style.color = "white";
+    newEnglishCell.style.background = "red";
+    newEnglishCell.style.color = "white";
+  }
+  state.errorRegister = false;
   newEnglishCell.textContent = state.answerSummary[1][0];
+
   newRow.appendChild(newSpanishCell);
   newRow.appendChild(newEnglishCell);
   tableBody.appendChild(newRow);
@@ -120,15 +207,16 @@ function updateSummaryTable() {
 
 function chickenDinner() {
     if (state.errorCounter === 0) {
+      questionText.textContent = "¡Bien hecho!";
         correctText.textContent = "PERFECTO";
         correctText.style.color = "green";
         correctCounter.style.color = "green";
         correctTen.style.color = "green";
     } else {
+      questionText.textContent = "Esfuérzate más";
         correctText.textContent = "Errores";
         correctCounter.textContent = state.errorCounter;
     }
-    verbTextQuestion.textContent = "¡Bien hecho!";
     titleText.textContent = "¡Richard habla español!";
     titleText.classList.replace("grey", "winner_winner_animation");
     setTimeout(() => {
@@ -139,39 +227,55 @@ function chickenDinner() {
 
 function checkAnswer(answer) {
   let inputFormatted = answer.toLowerCase().trim();
-  console.log("input formatted", inputFormatted);
-  console.log("index", state.verbIndex);
-  // get input against all answers in array
-  const answerIndex = verbs[state.verbIndex][1].findIndex((element) => {
-    return element.toLowerCase().trim() === inputFormatted;
-  });
+  // check input answer against all answers in array
+  let answerIndex
+  switch (state.currentTab) {
+    case "verbs":
+        answerIndex = verbs[state.questionIndex][1].findIndex((element) => {
+          return element.toLowerCase().trim() === inputFormatted;
+        });
+      break;
+    case "emotions": 
+        answerIndex = emotions[state.questionIndex][1].findIndex((element) => {
+        return element.toLowerCase().trim() === inputFormatted;
+      });
+    break;
+  }
 
   if (answerIndex !== -1) {
     // if any answer matches input
-    verbGreenCheck.style.color = "green";
-    verbGreenCheck.classList.add("green_check_animation");
+    greenCheck.style.color = "green";
+    greenCheck.classList.add("green_check_animation");
     setTimeout(() => {
-      verbGreenCheck.classList.remove("green_check_animation");
-      verbGreenCheck.style.color = "whitesmoke";
+      greenCheck.classList.remove("green_check_animation");
+      greenCheck.style.color = "whitesmoke";
     }, 1500);
-    verbs.splice(state.verbIndex, 1);
+    switch (state.currentTab) {
+      case "verbs":
+        verbs.splice(state.questionIndex, 1);
+        break;
+      case "emotions":
+        emotions.splice(state.questionIndex, 1);
+        break;
+    }
     state.correctCounter++;
     correctCounter.textContent = state.correctCounter;
-    verbTextAnswer.value = "";
+    answerText.value = "";
     if (state.correctCounter < 10) {
       updateSummaryTable();
-      updateVerbText();
+      updateQuestionText();
     } else {
       updateSummaryTable();
       chickenDinner();
     }
   } else {
-    verbRedCross.style.color = "red";
-    verbRedCross.classList.add("red_cross_animation");
+    redCross.style.color = "red";
+    redCross.classList.add("red_cross_animation");
     state.errorCounter++;
+    state.errorRegister = true
     setTimeout(() => {
-      verbRedCross.classList.remove("red_cross_animation");
-      verbRedCross.style.color = "whitesmoke";
+      redCross.classList.remove("red_cross_animation");
+      redCross.style.color = "whitesmoke";
     }, 1500);
   }
 }
@@ -193,4 +297,4 @@ function playSpanish(word) {
     speechSynthesis.speak(message);
 }
 
-updateVerbText();
+updateQuestionText();
